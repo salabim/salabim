59f962f8bf6c450fe59263d7387565a559d5830b
salabim <30370571+salabim@users.noreply.github.com>
2019-05-04 10:27:00 +0200

    19.0.4

--- install.py
+++ install.py
@@ -3,20 +3,21 @@ import os
 import shutil
 import glob
 import sys
-Pythonista = (sys.platform == 'ios')
 
-site_packages = os.sep + 'site-packages' + os.sep
+Pythonista = sys.platform == "ios"
+
+site_packages = os.sep + "site-packages" + os.sep
 
 if Pythonista:
-    documents = '/Documents'
+    documents = "/Documents"
     sp = os.getcwd().split(documents)
     if len(sp) != 2:
-        print('unable to install')
+        print("unable to install")
         exit()
-    path = sp[0] + documents + '/site-packages/salabim'
+    path = sp[0] + documents + "/site-packages/salabim"
 
 else:
-    path = numpy.__file__.split(site_packages)[0] + site_packages + 'salabim'
+    path = numpy.__file__.split(site_packages)[0] + site_packages + "salabim"
 
 if not os.path.isdir(path):
     os.makedirs(path)
@@ -25,13 +26,24 @@ files = glob.iglob("*.*")
 
 ok = False
 for file in files:
-    if (file in ('salabim.py', 'changelog.txt', 'license.txt')) or (file.endswith('.ttf')):
-        if file == 'salabim.py':
+    if (file in ("salabim.py", "changelog.txt", "license.txt")) or (file.endswith(".ttf")):
+        if file == "salabim.py":
             ok = True
         shutil.copy(file, path + os.sep + file)
 if ok:
-    with open(path + os.sep + '__init__.py', 'w') as initfile:
-        initfile.write('from .salabim import *\n')
-    print('salabim succesfully installed')
+    with open("salabim.py", "r") as f:
+        lines = f.read().splitlines()
+
+    realversion = ""
+    for line in lines:
+        a = line.split("__version__ = ")
+        if len(a) > 1:
+            realversion = a[1].replace("'", "")
+            realversion = a[1].replace('"', "")
+            break
+
+    with open(path + os.sep + "__init__.py", "w") as initfile:
+        initfile.write("from .salabim import *\n")
+    print("salabim " +realversion+ " succesfully installed")
 else:
-    print('could not find salabim.py')
+    print("could not find salabim.py")
