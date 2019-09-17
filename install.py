eca27a1573417c0df9a26b5c765ce1f6920dc27e
salabim <30370571+salabim@users.noreply.github.com>
2019-09-17 14:09:29 +0200

    19.0.8

--- install.py
+++ install.py
@@ -1,49 +1,59 @@
-import numpy
 import os
 import shutil
 import glob
 import sys
+import site
+import fnmatch
+
+includes = ("salabim.py", "changelog.txt", "license.txt", "*.ttf")
+mainfile = "salabim.py"
+package = "salabim"
 
 Pythonista = sys.platform == "ios"
 
-site_packages = os.sep + "site-packages" + os.sep
 
-if Pythonista:
-    documents = "/Documents"
-    sp = os.getcwd().split(documents)
-    if len(sp) != 2:
-        print("unable to install")
-        exit()
-    path = sp[0] + documents + "/site-packages/salabim"
+def main():
 
-else:
-    path = numpy.__file__.split(site_packages)[0] + site_packages + "salabim"
+    if Pythonista:
+        documents = "/Documents"
+        sp = os.getcwd().split(documents)
+        if len(sp) != 2:
+            print("unable to install")
+            exit()
+        path = f"{sp[0]}{documents}/site-packages/{package}"
 
-if not os.path.isdir(path):
-    os.makedirs(path)
+    else:
+        path = f"{site.getsitepackages()[-1]}{os.sep}{package}"
 
-files = glob.iglob("*.*")
+    if not os.path.isdir(path):
+        os.makedirs(path)
 
-ok = False
-for file in files:
-    if (file in ("salabim.py", "changelog.txt", "license.txt")) or (file.endswith(".ttf")):
-        if file == "salabim.py":
-            ok = True
-        shutil.copy(file, path + os.sep + file)
-if ok:
-    with open("salabim.py", "r") as f:
+    files = glob.iglob("*.*")
+
+    ok = False
+    for file in files:
+        if any(fnmatch.fnmatch(file, include) for include in includes):
+            if file == mainfile:
+                ok = True
+            shutil.copy(file, f"{path}{os.sep}{file}")
+    if not ok:
+        print(f"couldn't find {mainfile} in current directory")
+        return
+
+    with open(mainfile, "r") as f:
         lines = f.read().splitlines()
 
-    realversion = ""
+    realversion = "?"
     for line in lines:
         a = line.split("__version__ = ")
         if len(a) > 1:
-            realversion = a[1].replace("'", "")
-            realversion = a[1].replace('"', "")
+            realversion = a[1].replace("'", "").replace('"', "")
             break
 
-    with open(path + os.sep + "__init__.py", "w") as initfile:
-        initfile.write("from .salabim import *\n")
-    print("salabim " +realversion+ " succesfully installed")
-else:
-    print("could not find salabim.py")
+    with open(f"{path}{os.sep}__init__.py", "w") as initfile:
+        initfile.write(f"from .{package} import *\n")
+        initfile.write(f"from .{package} import __version__\n")
+    print(f"{package} {realversion} successfully installed")
+
+
+main()
